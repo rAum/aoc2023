12fed015e7f97cdb2f7092aeb61fa5d38a904052
Piotr Gródek <grodekpiotr+gh@gmail.com>
2023-12-02 18:40:42 +0100

    Fix stupid typo mistake

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -1,5 +1,3 @@
-use std::{cmp::max, collections::HashMap};
-
 fn main() {
     let input = include_str!("input.txt");
     let result = solution(input);
@@ -7,7 +5,10 @@ fn main() {
 }
 
 
-fn parse_line(line: &str) -> (usize, usize, usize, usize) {
+fn solve(line: &str) -> Option<usize> {
+    // red, blue, green
+    let limit: Vec<usize> = vec![12, 14, 13];
+
     // line has format Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     let game_rounds: Vec<&str> = line.split(":").collect();
     let game_num = game_rounds[0]
@@ -28,45 +29,21 @@ fn parse_line(line: &str) -> (usize, usize, usize, usize) {
             (n, color)
         }).collect();
 
-        // for v in parsed.iter() {
-        //     println!("Parsed: {:?}", v);
-        // }
+        for (n, color) in parsed.iter() {
+            if *n > limit[*color] {
+                return None;
+            }
+        }
         parsed_rounds.push(parsed);
     }
-
-    let mut colors = HashMap::new();
-    for i in 0..3 {
-        colors.insert(i, 0);
-    }
-    
-    // pick max color appearing in rounds
-    for (n, color) in parsed_rounds.into_iter().flatten() {
-        let color_count = colors.entry(color).or_default();
-        *color_count = max(*color_count, n);
-    }
-
-    println!("Max colors for game {}: {:?}", game_num, colors);
-
-    let r = *colors.get(&0).expect("red should be present");
-    let b = *colors.get(&1).expect("blue should be present");
-    let g = *colors.get(&2).expect("green should be present");
-
-    (r, b, g, game_num)
+    Some(game_num)
 }
 
 fn solution(input: &str) -> usize {
-    // red, blue, green
-    let limit = (12, 13, 14);
-
     let result = input
         .lines()
-        .map(parse_line)
-        .filter_map(|game| {
-            if game.0 > limit.0 || game.1 > limit.1 || game.2 > limit.2 {
-                return None;
-            }
-            Some(game.3)
-        }).inspect(|game_n| {
+        .filter_map(solve)
+        .inspect(|game_n| {
             println!("Game {} is okay", game_n);
         }).sum();
         result
