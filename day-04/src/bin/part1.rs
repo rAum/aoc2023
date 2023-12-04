7b2dac022d19b3263901076047a7f88c3827b591
Piotr Gródek <grodekpiotr+gh@gmail.com>
2023-12-04 08:56:19 +0100

    Solution for day 5 part 1

--- day-04/src/bin/part1.rs
+++ day-04/src/bin/part1.rs
@@ -6,8 +6,34 @@ fn main() {
     println!("Result: {}", result);
 }
 
+
 fn solution(input: &str) -> u32 {
-    todo!();
+    input.lines().filter_map(|line| -> Option<u32> {
+        let card_sequnces: Vec<_> = line.split(":").collect();
+        let _card = card_sequnces.iter().nth(0).unwrap();    
+        let sequences: Vec<_> = card_sequnces.iter().nth(1).unwrap().split("|").collect();
+
+        let winning_numbers: HashSet<u32> = sequences[0]
+        .split_whitespace()
+        .map(|txt| -> u32 {
+            txt.trim().to_string().parse::<u32>().unwrap()
+        }).collect();
+
+        let winning_tickets = sequences[1]
+        .split_whitespace()
+        .filter_map(|txt| {
+            let v = txt.trim().to_string().parse::<u32>().unwrap();
+            if winning_numbers.contains(&v) {
+                return Some(v)
+            }
+            None
+        }).count();
+        let score: u32 = match winning_tickets {
+            0 => 0,
+            v => 2u32.pow(v as u32 - 1),
+        };
+        Some(score)
+    }).sum()
 }
 
 #[cfg(test)]
@@ -17,7 +43,7 @@ mod tests {
     #[test]
     fn test() {
         let test_input = include_str!("test1.txt");
-        let expected_result = 4361;
+        let expected_result = 13;
 
         let result = solution(test_input);
 
